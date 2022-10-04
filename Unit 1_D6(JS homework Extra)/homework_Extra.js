49ec5611bc4165827c8e7000141fc25551a6e5d6
Louis Gadza <siuolgadza@gmail.com>
2022-10-04 16:13:57 +0200

    Unit 1_D6(JS)

---
+++ Unit 1_D6(JS homework Extra)/homework_Extra.js
@@ -0,0 +1,22 @@
+// Homework [EXTRA]
+
+// ingredients
+let spaghetti = 400;
+let guanciale = 250;
+let eggYolk = 6;
+let pecorinoRomano = 50;
+let blackPepper = 4;
+
+// preparation
+
+let firstMix = eggYolk + pecorinoRomano;
+let secondMix = firstMix + (1 / 4) * blackPepper;
+let thirdMix = guanciale + spaghetti;
+let forthMix = thirdMix + secondMix;
+let past = "add spaghetti in  salty water then drain the water once cooked";
+let creamySauce = "add 2 spoons of the served water inot guanciale";
+
+let carbonara = forthMix + (3 / 4) * blackPepper;
+// Spaghetti alla Carbonara read to be served
+console.log(carbonara);
+console.log("Enjoy your 'Spaghetti alla Carbonara' :)");
---
+++ homework.js.js
@@ -0,0 +1,10 @@
+// Homework
+let firstName = "Louis";
+let lastName = "Gadza";
+let fullName = firstName + " " + lastName;
+// a better way will be
+// let fullName = `${firstName} ${lastName}`;
+console.log(`hello, my name is ${fullName}.`);
+let myAge = 24;
+let currentYr = 2022;
+let myBirthYr = console.log(currentYr - myAge);
