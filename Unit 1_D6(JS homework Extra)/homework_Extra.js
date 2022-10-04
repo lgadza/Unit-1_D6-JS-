b741d93bbfcfeeddae686ab99f3bdef51964ba3d
Louis Gadza <siuolgadza@gmail.com>
2022-10-04 17:38:45 +0200

    homework

--- Unit 1_D6(JS homework Extra)/homework_Extra.js
+++ Unit 1_D6(JS homework Extra)/homework_Extra.js
@@ -10,13 +10,20 @@ let blackPepper = 4;
 // preparation
 
 let firstMix = eggYolk + pecorinoRomano;
+console.log(`first mix: ${firstMix}`);
 let secondMix = firstMix + (1 / 4) * blackPepper;
+console.log(`secondMix: ${secondMix}`);
+
 let thirdMix = guanciale + spaghetti;
+console.log(`thirdMix: ${thirdMix}`);
+
 let forthMix = thirdMix + secondMix;
+console.log(`forthMix: ${forthMix}`);
+
 let past = "add spaghetti in  salty water then drain the water once cooked";
 let creamySauce = "add 2 spoons of the served water inot guanciale";
 
 let carbonara = forthMix + (3 / 4) * blackPepper;
 // Spaghetti alla Carbonara read to be served
-console.log(carbonara);
+console.log(`carbonara: ${carbonara}`);
 console.log("Enjoy your 'Spaghetti alla Carbonara' :)");
